488eb03bf45fba7c8f6f01b1944e3c78c03e2ba6
wangxiaofeng <wang.xiaofeng@synyi.com>
2020-07-16 15:30:45 +0800

    算法

---
+++ AtrCode/拥有最多糖果的孩子/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace 拥有最多糖果的孩子
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var candies =new int[]{ 2, 3, 5, 1, 3 };
+            var extraCandies = 3;
+            var result=KidsWithCandies(candies, extraCandies);
+            foreach (var b in result)
+            {
+                Console.WriteLine(b.ToString());
+            }
+            Console.ReadKey();
+        }
+        public static IEnumerable<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            foreach (var candy in candies)
+            {
+                var currentvalue = candy + extraCandies;
+
+                var Maxds = candies.Max();
+
+                if (currentvalue >= Maxds)
+                    yield return true;
+                else
+                {
+                    yield return false;
+                }
+
+            }
+        }
+    }
+}
---
+++ AtrCode/斐波那契/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 斐波那契
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = V3(5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var r = feibonaqie(i+1);
+                Console.WriteLine(r);
+            }
+             
+            //foreach (var VARIABLE in t)
+            //{
+            //    Console.WriteLine(VARIABLE);
+            //}
+            
+           
+            Console.ReadKey();
+        }
+
+        private static int feibonaqie(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n == 2)
+            {
+                return 1;
+            }
+
+           var result = feibonaqie(n - 1) + feibonaqie(n - 2);
+
+
+           return result;
+        }
+
+
+        private static IEnumerable<int> V3(int number)
+        {
+            Console.WriteLine("V3");
+            int a = 0, b = 1, c = 0;
+            for (int i = 0; i < number; i++)
+            {
+                yield return b;
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
