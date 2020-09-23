e3ed57bef61eda8975a6207f4d863543394929c9
XiaofengWang <1223138295@qq.com>
2020-09-23 20:52:06 +0800

    lock

---
+++ Task/Reflection/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Reflection.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(IServiceProvider serviceProvider, ILogger<WeatherForecastController> logger)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var types = typeof(TestRef);
+
+            var obj=_serviceProvider.GetService(types);
+
+            var tt = typeof(IIntertest);
+
+            var  tts=tt.GetMethod("Test").Invoke(obj,null);
+
+            return tts.ToString();
+        }
+    }
+
+    public class TestRef:IIntertest
+    {
+        public string Test()
+        {
+            return "sss";
+        }
+    }
+}
---
+++ lock/locksln/locktest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace locktest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        private static int a;
+        [HttpGet]
+        public int Get()
+        {
+            Parallel.For(0, 1000, (i) =>
+            {
+                Interlocked.Increment(ref a);
+            });
+            
+            
+            var rng = new Random();
+            return a;
+        }
+    }
+}
